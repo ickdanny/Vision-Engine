83eecbc54b8d587d7286230a6bd7368bcc55d37b
ickdanny <fake_email@gmail.com>
2026-06-10 18:27:29 -0400

    Updated build scripts to fully build the executable.

--- build.ps1
+++ build.ps1
@@ -1,3 +1,26 @@
 $ErrorActionPreference = "Stop"
 
-#todo
+# Clean
+Remove-Item -Recurse -Force build,.gradle,VOSG,VOSG.zip -ErrorAction SilentlyContinue
+
+# Build
+.\gradlew.bat build
+
+# Create runtime image
+jlink --add-modules java.base,java.desktop --output build\runtime
+
+# Package
+jpackage `
+  --type app-image `
+  --name VOSG `
+  --input .\build\libs `
+  --main-jar VOSG.jar `
+  --runtime-image .\build\runtime `
+  --icon .\packaging\icon.ico
+
+# Copy resources and packaging
+cp -r res VOSG\
+cp -r packaging\* VOSG\
+
+# Zip it up
+Compress-Archive -Path VOSG -DestinationPath VOSG.zip
